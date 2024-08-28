be72f203ec69c3646cace09fb8bb848ecc8d3269
PrajaktaKambale <prajaktakamble42@gmail.com>
2024-08-28 15:17:55 +0530

    conn file chnaged

--- conn/conn.js
+++ conn/conn.js
@@ -1,14 +1,12 @@
-const mongoose = require("mongoose");
+import { connect } from "mongoose";
 
 const conn = async (req, res) => {
   try {
-    await mongoose
-      .connect(
-        "mongodb+srv://prajaktakambale2110:Test%4012345@appmanagement.uwpzd.mongodb.net/bookmanagementdatabase?retryWrites=true&w=majority"
-      )
-      .then(() => {
-        console.log("Connected");
-      });
+    await connect(
+      "mongodb+srv://prajaktakambale2110:Test%4012345@appmanagement.uwpzd.mongodb.net/bookmanagementdatabase?retryWrites=true&w=majority"
+    ).then(() => {
+      console.log("Connected");
+    });
   } catch (error) {
     console.log(error);
   }
