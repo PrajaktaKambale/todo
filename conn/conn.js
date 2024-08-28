86d22ec970c40992c582c7fae3f1c2164221629c
PrajaktaKambale <prajaktakamble42@gmail.com>
2024-08-28 15:17:23 +0530

    database url changed

--- conn/conn.js
+++ conn/conn.js
@@ -4,7 +4,7 @@ const conn = async (req, res) => {
   try {
     await mongoose
       .connect(
-        ""
+        "mongodb+srv://prajaktakambale2110:Test%4012345@appmanagement.uwpzd.mongodb.net/bookmanagementdatabase?retryWrites=true&w=majority"
       )
       .then(() => {
         console.log("Connected");
